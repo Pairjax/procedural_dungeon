81a6bc3e47f7d213891ae96eb01ec7110361ff54
Austin Smith <pairjax@protonmail.com>
2023-06-07 14:38:27 -0400

    Assimp Added
    - Added assimp to cmake

--- procedural_dungeon/engine/model.h
+++ procedural_dungeon/engine/model.h
@@ -11,17 +11,17 @@ class Model
 public:
     Model(char* path)
     {
-        loadModel(path);
+        // loadModel(path);
     }
     void Draw(Shader& shader);
 private:
     // model data
-    vector<Mesh> meshes;
+    /*vector<Mesh> meshes;
     string directory;
 
     void loadModel(string path);
     void processNode(aiNode* node, const aiScene* scene);
     Mesh processMesh(aiMesh* mesh, const aiScene* scene);
     vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
-        string typeName);
+        string typeName);*/
 };
