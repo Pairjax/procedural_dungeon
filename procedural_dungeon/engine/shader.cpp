65f7f3d555fea4608113027ecc9b3e34026a116c
Austin Smith <pairjax@protonmail.com>
2023-06-07 11:42:53 -0400

    GLM added w/ Shader code
    - Added glm to cmake
    - Created shader class w/ importable shader files

---
+++ procedural_dungeon/engine/shader.cpp
@@ -0,0 +1,93 @@
+#include "shader.h"
+
+Shader::Shader(const char* vertex_path, const char* fragment_path) {
+    std::string vertex_code;
+    std::string fragment_code;
+    std::ifstream vertex_shader_file;
+    std::ifstream fragment_shader_file;
+
+    // Setup shader exceptions
+    vertex_shader_file.exceptions(std::ifstream::badbit);
+    fragment_shader_file.exceptions(std::ifstream::badbit);
+
+    // Capture input stream into string
+    try {
+        vertex_shader_file.open(vertex_path);
+        fragment_shader_file.open(fragment_path);
+
+        std::stringstream vertex_shader_stream, fragment_shader_stream;
+        vertex_shader_stream << vertex_shader_file.rdbuf();
+        fragment_shader_stream << fragment_shader_file.rdbuf();
+
+        vertex_shader_file.close();
+        fragment_shader_file.close();
+
+        vertex_code = vertex_shader_stream.str();
+        fragment_code = fragment_shader_stream.str();
+    } catch (std::ifstream::failure e) {
+        std::cout << "File not successfully read!" << std::endl;
+    }
+
+    const char* vertex_shader_code = vertex_code.c_str();
+    const char* fragment_shader_code = fragment_code.c_str();
+
+    // Compile Shaders
+    unsigned int vertex_shader, fragment_shader;
+    int success;
+    char info_log[512];
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_code, NULL);
+    glCompileShader(vertex_shader);
+
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        std::cout << "Vertex Shader Compilation Failed! Reason: " << std::endl
+            << info_log << std::endl;
+    };
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_code, NULL);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        std::cout << "Fragment Shader Compilation Failed! Reason: " << std::endl
+            << info_log << std::endl;
+    };
+
+    // Create Shader Program & Link Shaders
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex_shader);
+    glAttachShader(ID, fragment_shader);
+    glLinkProgram(ID);
+
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(ID, 512, NULL, info_log);
+        std::cout << "Shader Linking Failed! Reason: " << std::endl
+            << info_log << std::endl;
+    }
+
+    // No longer necessary; linked to program
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+void Shader::use() {
+    glUseProgram(ID);
+}
+
+void Shader::set_bool(const std::string& name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
+}
+
+void Shader::set_int(const std::string& name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::set_float(const std::string& name, float value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
---
+++ procedural_dungeon/engine/shader.h
@@ -0,0 +1,22 @@
+#include <GL/glew.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader {
+public:
+    // Shader Program ID
+    unsigned int ID;
+
+    // Reads & Builds Shader
+    Shader(const char* vertex_path, const char* fragment_path);
+    // Use/activate the shader
+    void use();
+
+    // Sets values for the shader uniform
+    void set_bool(const std::string& name, bool value) const;
+    void set_int(const std::string& name, int value) const;
+    void set_float(const std::string& name, float value) const;
+};
